5c5b525d9d54930593509d9839cfa0506b8c6a69
graham2chow@gmail.com <graham2chow@gmail.com>
2016-02-07 12:39:38 +1100

    t

--- scratch_experiment.js
+++ scratch_experiment.js
@@ -93,9 +93,9 @@ new (function() {
        return _gyro_z;
     };
     
-    var port = chrome.runtime.connect('pcajcocaacnogeggjaelfcbnnbblojod', {name: "knockknock"});
+    var port = chrome.runtime.connect('pcajcocaacnogeggjaelfcbnnbblojod');
     console.log('connect');
-    port.postMessage({joke: "Knock knock"});
+    port.postMessage('Knock knock');
     port.onMessage.addListener(function(msg) {
         console.log('joke message');
         if (msg.question == "Who's there?")
